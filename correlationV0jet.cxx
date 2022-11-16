50eabd7a97a98861746ec32ee36c9fecff632468
Johanna Lomker <jloemker@yahoo.de>
2022-11-16 15:50:29 +0100

    trying to put pieces together without any success

--- correlationV0jet.cxx
+++ correlationV0jet.cxx
@@ -50,7 +50,7 @@ using namespace o2::framework::expressions;
 using MyTracks = soa::Join<aod::Tracks, aod::TracksExtra, aod::pidTPCPi, aod::pidTPCPr>;
 using MyTracksRun2 = soa::Join<aod::Tracks, aod::TracksExtra, aod::TracksCov, aod::TracksDCA, aod::pidTPCPi, aod::pidTPCPr>;//run 2 data stores Tracks in AO2D
 using MyTracksRun3 = soa::Join<aod::TracksIU, aod::TracksExtra, aod::TracksCovIU, aod::TracksDCA, aod::pidTPCPi, aod::pidTPCPr>;//run 3 data stores TracksIU (inner most update) in AO2D
-using LabeledV0s = soa::Join<aod::V0Datas, aod::McV0Labels>;
+//using LabeledV0s = soa::Join<aod::V0Datas, aod::McV0Labels>;
 
 struct vzerotemplateexample {
   //Configurable for number of bins
@@ -86,7 +86,7 @@ struct vzerotemplateexample {
 
 
 
-  // Jet and JetConstituents are tables which are filled by executable o2-analysis-jetfinder
+  /* Jet and JetConstituents are tables which are filled by executable o2-analysis-jetfinder
   void getJets(aod::Jet const& jet, aod::JetTrackConstituents const& constituents, aod::Tracks const& tracks)
   {
     registry.fill(HIST("jetPt"),jet.pt());
@@ -94,7 +94,7 @@ struct vzerotemplateexample {
       LOGF(debug, "jet %d: track id %d, track pt %g", jet.index(), c.trackId(), c.track().pt());
       registry.fill(HIST("constituentPt"), c.track().pt());
     }
-  }
+  }*/
 
 
   template <class TMyTracks, typename TV0>
@@ -119,14 +119,14 @@ struct vzerotemplateexample {
        registry.fill(HIST("hMAntiLambda"), v0.mAntiLambda());
      }   
      // for this i need an additional .h file and another subscription in my pipeline !
-     //An adding the final STEP *5* - lambdakzerobuilder is able to do MC association for me #wow !
+     /*An adding the final STEP *5* - lambdakzerobuilder is able to do MC association for me #wow !
      if( v0.has_mcParticle()){//soem association was made !
        auto v0mcparticle = v0.mcParticle();
        //Check particle PDG code to see if this is the one you want
        if( v0mcparticle.pdgCode() == 310 ) registry.fill(HIST("hMassTrueK0Short"), v0.mK0Short());
        if( v0mcparticle.pdgCode() == 3122 ) registry.fill(HIST("hMassTrueLambda"), v0.mLambda());// wtf why the same odg code for lamda and anti lambda ??
        if( v0mcparticle.pdgCode() == 3122 ) registry.fill(HIST("hMassTrueAntiLambda"), v0.mAntiLambda());
-     }
+     }*/
    }
   }
   //*
@@ -137,8 +137,8 @@ struct vzerotemplateexample {
     // (...)
     //check getter here: https://aliceo2group.github.io/analysis-framework/docs/datamodel/ao2dTables.html
   //*
-  //Process function for old data (run2)
-  void processRun2(soa::Join<aod::Collisions, aod::EvSels>::iterator const& collision, soa::Filtered<LabeledV0s> const& V0s, MyTracksRun2 const& tracks, aod::McParticles const&)
+  //Process function for old data (run2)//change from LabeldV0s to Vo0Datas and removed Mcparticle
+  void processRun2(soa::Join<aod::Collisions, aod::EvSels>::iterator const& collision, soa::Filtered<aod::V0Datas> const& V0s, MyTracksRun2 const& tracks)
   {
     if( !collision.sel7()){//sel7=bool -> Event selection decision based on V0A & V0C + all helper tasks required for this !
       return;
@@ -148,9 +148,9 @@ struct vzerotemplateexample {
       processV0Candidate<MyTracksRun2>(v0, collision.posX(), collision.posY(), collision.posZ() );
     }
   }
-  PROCESS_SWITCH(vzerotemplateexample, processRun2, "Process Run 2 data", false);
+  PROCESS_SWITCH(vzerotemplateexample, processRun2, "Process Run 2 data", true);
   //Define process function for run3 data
-  void processRun3(soa::Join<aod::Collisions, aod::EvSels>::iterator const& collision, soa::Filtered<LabeledV0s> const& V0s, MyTracksRun3 const& tracks, aod::McParticles const&)
+  void processRun3(soa::Join<aod::Collisions, aod::EvSels>::iterator const& collision, soa::Filtered<aod::V0Datas> const& V0s, MyTracksRun3 const& tracks)
   {
     if( !collision.sel8()){//sel8=bool -> Event selection decision based on TVX) ! Event selection is based on a different detector part for run2 / run3 !
       return;
@@ -160,70 +160,63 @@ struct vzerotemplateexample {
       processV0Candidate<MyTracksRun3>(v0, collision.posX(), collision.posY(), collision.posZ());
     }
   }
-  PROCESS_SWITCH(vzerotemplateexample, processRun3, "Process Run 3 data", true);
+  PROCESS_SWITCH(vzerotemplateexample, processRun3, "Process Run 3 data", false);
 
 };
-//
-//
-//either I merge all in one struct or I have to include output tables - at some point i have to do this anyway...
-//
-struct HistogramTrackSelection {
-
-  Configurable<int> selectedTracks{"select", 1, "Choice of track selection. 0 = no selection, 1 = globalTracks, 2 = globalTracksSDD"};
-
-  OutputObj<TH1F> pt{TH1F("pt", "pt", 100, 0., 50.)};
-
-  void init(o2::framework::InitContext&) {}
-
-  // group tracks according to collision
-  void process(aod::Collision const& collision,
-               soa::Join<aod::Tracks, aod::TrackSelection> const& tracks)
-  {
-    for (auto& track : tracks) {
-
-      // select tracks according to configurable selectedTracks
-      if (selectedTracks == 1 && !track.isGlobalTrack()) {
-        continue;
-      } else if (selectedTracks == 2 && !track.isGlobalTrackSDD()) {
-        continue;
-      }
-
-      // fill pt histogram
-      pt->Fill(track.pt());
-    }
-  }
-};
-
 
 struct Jets{
-  OutputObj<TH1F> hJetPt{"jetPt"};
-  OutputObj<TH1F> hConstPt{"constPt"};
+  Configurable<int> nBins{"nBins", 100, "N bins in all histos"};
+  HistogramRegistry registry{
+    "registry",
+    {
+      {"hVtxZ", "hVtxZ", {HistType::kTH1F, {{nBins, -15., 15.}}}},
+      {"hMK0Short", "hMK0Short", {HistType::kTH1F, {{200,0.450f,0.550f}}}},//why these exact numbers - around mass peak i assume :P
+      {"hMLambda", "hMLambda", {HistType::kTH1F, {{200, 0, 10}}}},
+      {"hMAntiLambda", "hMAntiLambda", {HistType::kTH1F, {{200, 0, 10}}}},
+      {"hMassTrueK0Short", "hMassTrueK0Short", {HistType::kTH1F, {{200,0.450f,0.550f}}}},
+      {"hMassTrueLambda", "hMassTrueLambda", {HistType::kTH1F, {{200, 0, 10}}}},
+      {"hMassTrueAntiLambda", "hMassTrueAntiLambda", {HistType::kTH1F, {{200, 0, 10}}}},
 
-  void init(InitContext const&)
-  {
-    hJetPt.setObject(new TH1F("jetPt", "jet p_{T};p_{T} (GeV/#it{c})",
-                              100, 0., 100.));
-    hConstPt.setObject(new TH1F("constPt", "constituent p_{T};p_{T} (GeV/#it{c})",
-                                100, 0., 100.));
-  }
+      {"jetPt", "jet p_{T};p_{T} (GeV/#it{c})", {HistType::kTH1F, {{100, 0, 100}}}},
+      {"constPt", "constituent p_{T};p_{T} (GeV/#it{c})", {HistType::kTH1F, {{100, 0, 100}}}}
+    }
+  };
 
   // Jet and JetConstituents are tables which are filled by executable o2-analysis-jetfinder
-  void process(aod::Jet const& jet,
-               aod::JetTrackConstituents const& constituents, aod::Tracks const& tracks)
+  void process(aod::Jet const& jet, aod::JetTrackConstituents const& constituents, aod::Tracks const& tracks)
   {
-    hJetPt->Fill(jet.pt());
+    registry.fill(HIST("jetPt"), jet.pt());
     for (const auto& c : constituents) {
       LOGF(debug, "jet %d: track id %d, track pt %g", jet.index(), c.trackId(), c.track().pt());
-      hConstPt->Fill(c.track().pt());
+      registry.fill(HIST("constPt"), c.track().pt());
+    }
+    for(auto& track : tracks ){
+      if(track.has_collision()){
+       // V0(track.collision(), V0s);
+        
+
+      }
     }
   }
+
+  void V0(soa::Join<aod::Collisions, aod::EvSels>::iterator const& collision, aod::V0Datas const& V0s){
+     //now getting V0
+    if(!collision.sel8()){//event selection based on TVX -> https://aliceo2group.github.io/analysis-framework/docs/datamodel/helperTaskTables.html
+      return;
+     }
+     registry.fill(HIST("hVertexZ"),collision.posZ());
+     for(auto& v0 : V0s){
+       registry.fill(HIST("hMassK0Short"), v0.mK0Short());
+     }
+   }
 };
+
 
 
 WorkflowSpec defineDataProcessing(ConfigContext const& cfgc)
 {
   return WorkflowSpec{//run all via , separation
-    adaptAnalysisTask<vzerotemplateexample>(cfgc)
-   // adaptAnalysisTask<Jets>(cfgc)
+   // adaptAnalysisTask<vzerotemplateexample>(cfgc)
+   adaptAnalysisTask<Jets>(cfgc)
   };
 }
