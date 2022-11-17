872f34945c8577b6b45289743e3e52ccfd6d7453
Johanna Lomker <jloemker@yahoo.de>
2022-11-17 21:46:16 +0100

    adding kinematics of V0 and tracks at different stages

--- correlationV0jet.cxx
+++ correlationV0jet.cxx
@@ -67,12 +67,18 @@ struct correlationvzerojets{
   Configurable<float> trackEtaCut{"trackEtaCut", 0.9, "constituent eta cut"};
 
   //configurables for the V0 identification of lambda and K0short through  selection criteria on daughters
-  Configurable<double> v0cospa{"v0cospa", 0.97, "V0 CosPA" };// double -> N.B. dcos(x)/dx = 0 at x=0
+  //you should really kniow wat they are ...
+  Configurable<double> v0cospa{"v0cospa", 0.97, "V0 CosPA" };// double -> N.B. dcos(x)/dx = 0 at x=0  what is this ??
   Configurable<float> dcav0dau{"dcav0dau", 1.0, "DCA V0 Daughters"};
   Configurable<float> dcanegtopv{"dcanegtopv", -1, "DCA Neg To PV"};
   Configurable<float> dcapostopv{"dcapostopv", -1, "DCA Pos To PV"};//why would they also put -1: bc this is the initiaization, updated in process du dummy 
-  Configurable<float> v0radius{"v0radius", 0.5, "v0radius"};
-  Configurable<int> nBins{"nBins", 100, "N bins in all pT histos"};
+  Configurable<float> v0radius{"v0radius", 0.5, "v0radius"};//what is this ? decay cone ? no idea
+
+  Configurable<int> nBins{"nBins", 100, "N bins in histos"};
+  Configurable<int> nBinsPt{"nBinsPt", 100, "N bins in pT histos"};
+  Configurable<int> nBinsEta{"nBinsEta", 100, "N bins in Eta histos"};
+  Configurable<int> nBinsPhi{"nBinsPhi", 100, "N bins in Phi histos"};
+
 
   Filter collisionFilter = nabs(aod::collision::posZ) < vertexZCut;
   Filter trackFilter = (nabs(aod::track::eta) < trackEtaCut) && (requireGlobalTrackInFilter()) && (aod::track::pt > trackPtCut);
@@ -80,16 +86,38 @@ struct correlationvzerojets{
   HistogramRegistry registry{
     "registry",
     {
-      {"hVtxZ", "hVtxZ", {HistType::kTH1F, {{nBins, -15., 15.}}}},
-      {"hMK0Short", "hMK0Short", {HistType::kTH1F, {{200,0.450f,0.550f}}}},//why these exact numbers - around mass peak i assume :P
-      {"hMLambda", "hMLambda", {HistType::kTH1F, {{200, 0, 10}}}},
-      {"hMAntiLambda", "hMAntiLambda", {HistType::kTH1F, {{200, 0, 10}}}},
-      {"hTrackPt", "track p_{T}; p_{T} (GeV/#it{c})", {HistType::kTH1F, {{nBins, 0, 100}}}},
-      {"hPtLambda", "v0.ptlambda p_{T}; p_{T} (GeV/#it{c})", {HistType::kTH1F, {{nBins, 0, 100}}}},
-      {"hPtTrackV0", "v0.pt p_{T}; p_{T} (GeV/#it{c})", {HistType::kTH1F, {{nBins, 0, 100}}}},
+      {"hCollVtxZ", "hCollisionVtxZ", {HistType::kTH1F, {{nBins, -15., 15.}}}},
 
-      {"jetPt", "jet p_{T};p_{T} (GeV/#it{c})", {HistType::kTH1F, {{100, 0, 100}}}},
-      {"constTrackPt", "constituent track p_{T};p_{T} (GeV/#it{c})", {HistType::kTH1F, {{100, 0, 100}}}}
+      {"hMK0Short", "hMK0Short", {HistType::kTH1F, {{200,0.450f,0.550f}}}},//why these exact numbers - around mass peak i assume :P
+      {"hPtK0Short", "if K0Short : v0.pt()  p_{T}; p_{T} (GeV/#it{c})", {HistType::kTH1F, {{nBinsPt, 0, 100}}}},
+      {"hEtaK0Short", "if K0Short : v0.eta() #eta; #eta", {HistType::kTH1F, {{nBinsEta, -0.9, 0.9}}}},
+      {"hPhiK0Short", "if K0Short : v0.phi() #phi; #phi", {HistType::kTH1F, {{nBinsPhi, 0, 6.3}}}},
+
+      {"hMLambda", "hMLambda", {HistType::kTH1F, {{200, 0, 10}}}},
+      {"hPtLambda", "if Lambda : v0.pt()  p_{T}; p_{T} (GeV/#it{c})", {HistType::kTH1F, {{nBinsPt, 0, 100}}}},
+      {"hEtaLambda", "if Lambda : v0.eta() #eta; #eta", {HistType::kTH1F, {{nBinsEta, -0.9, 0.9}}}},
+      {"hPhiLambda", "if Lambda : v0.phi() #phi; #phi", {HistType::kTH1F, {{nBinsPhi, 0, 6.3}}}},
+
+      {"hMAntiLambda", "hMAntiLambda", {HistType::kTH1F, {{200, 0, 10}}}},
+      {"hPtAntiLambda", "if AntiLambda : v0.pt()  p_{T}; p_{T} (GeV/#it{c})", {HistType::kTH1F, {{nBinsPt, 0, 100}}}},
+      {"hEtaAntiLambda", "if AntiLambda : v0.eta() #eta; #eta", {HistType::kTH1F, {{nBinsEta, -0.9, 0.9}}}},
+      {"hPhiAntiLambda", "if AntiLambda : v0.phi() #phi; #phi", {HistType::kTH1F, {{nBinsPhi, 0, 6.3}}}},      
+      //control plots	
+      {"hPtTrackV0inRadius", "V0 from V0Datas in V0 radius  p_{T}; p_{T} (GeV/#it{c})", {HistType::kTH1F, {{nBins, 0, 100}}}},
+      {"hEtaTrackV0inRadius", "V0 from V0Datas in V0 radius #eta; #eta", {HistType::kTH1F, {{nBinsEta, -0.9, 0.9}}}},
+      {"hPhiTrackV0inRadius", "V0 from V0Datas in V0 radius  #phi; #phi", {HistType::kTH1F, {{nBinsPhi, 0, 6.3}}}},
+
+      {"hPtV0", "V0 in V0Datas p_{T}; p_{T} (GeV/#it{c})", {HistType::kTH1F, {{nBinsPt, 0, 100}}}},
+      {"hEtaV0", "V0 in V0Datas #eta; #eta", {HistType::kTH1F, {{nBinsEta, -0.9, 0.9}}}},
+      {"hPhiV0", "V0 in V0Datas  #phi; #phi", {HistType::kTH1F, {{nBinsPhi, 0, 6.3}}}},
+
+      {"hTrackPt", "MyTracks in collision track p_{T}; p_{T} (GeV/#it{c})", {HistType::kTH1F, {{nBinsPt, 0, 100}}}},
+      {"hTrackEta", "MyTracks in collision: track #eta; #eta", {HistType::kTH1F, {{nBinsEta, -0.9, 0.9}}}},
+      {"hTrackPhi", "MyTracks in collision: track #phi; #phi", {HistType::kTH1F, {{nBinsPhi, 0, 6.3}}}},
+
+      //jets as next
+      {"jetPt", "jet p_{T};p_{T} (GeV/#it{c})", {HistType::kTH1F, {{nBinsPt, 0, 100}}}},
+      {"constTrackPt", "constituent track p_{T};p_{T} (GeV/#it{c})", {HistType::kTH1F, {{nBinsPt, 0, 100}}}}
 
     }
   };
@@ -108,35 +136,50 @@ struct correlationvzerojets{
   void V0(soa::Filtered<soa::Join<aod::Collisions, aod::EvSels>>::iterator const& collision, MyTracks const& tracks, aod::V0Datas const& V0s){
     if(!collision.sel7()){// sel8 is event selection based on TVX mainly for run3 data, but sel7=bool -> Event selection decision based on V0A & V0C -> https://aliceo2group.github.io/analysis-framework/docs/datamodel/helperTaskTables.html
       return;
-     }
+    }
 
-     registry.fill(HIST("hVtxZ"),collision.posZ()); // Inclusive Tracks from sel7 selections and the aod::pidTPCPi, aod::pidTPCPr
+    registry.fill(HIST("hCollVtxZ"),collision.posZ()); // Inclusive Tracks from sel7 selections and the aod::pidTPCPi, aod::pidTPCPr
 	
-      for(auto& v0 : V0s){
-	// particle identification by using the tpcNSigma track variable
-        float nsigma_pos_proton = TMath::Abs(v0.posTrack_as<MyTracks>().tpcNSigmaPr());
-        float nsigma_neg_proton = TMath::Abs(v0.negTrack_as<MyTracks>().tpcNSigmaPr());
-        float nsigma_pos_pion = TMath::Abs(v0.posTrack_as<MyTracks>().tpcNSigmaPi());
-        float nsigma_neg_pion = TMath::Abs(v0.negTrack_as<MyTracks>().tpcNSigmaPi());
-
-        if(v0.v0radius() > v0radius && v0.v0cosPA(collision.posX(), collision.posY(), collision.posZ()) > v0cospa ){
-          if( nsigma_pos_pion < 4 && nsigma_neg_pion < 4 ){
-	    registry.fill(HIST("hMK0Short"), v0.mK0Short());
-	  }
-	  if( nsigma_pos_proton < 4 && nsigma_neg_proton < 4){
-            registry.fill(HIST("hMLambda"), v0.mLambda());
-	  }
-	  if( nsigma_pos_pion < 4 && nsigma_neg_proton < 4){
-            registry.fill(HIST("hMAntiLambda"), v0.mAntiLambda());	
-	  }
-        // registry.fill(HIST("hPtLambda"), v0.Lambda());
-        registry.fill(HIST("hPtTrackV0"), v0.pt());
-	
-      }
-
+    for(auto& v0 : V0s){
+      // particle identification by using the tpcNSigma track variable
+      float nsigma_pos_proton = TMath::Abs(v0.posTrack_as<MyTracks>().tpcNSigmaPr());//what does tpcNSigmaPr / Pi exactly ??
+      float nsigma_neg_proton = TMath::Abs(v0.negTrack_as<MyTracks>().tpcNSigmaPr());
+      float nsigma_pos_pion = TMath::Abs(v0.posTrack_as<MyTracks>().tpcNSigmaPi());
+      float nsigma_neg_pion = TMath::Abs(v0.negTrack_as<MyTracks>().tpcNSigmaPi());
+      
+      if(v0.v0radius() > v0radius && v0.v0cosPA(collision.posX(), collision.posY(), collision.posZ()) > v0cospa ){//i dont understand this criteria 
+        if( nsigma_pos_pion < 4 && nsigma_neg_pion < 4 ){
+          registry.fill(HIST("hMK0Short"), v0.mK0Short());
+	  registry.fill(HIST("hPtK0Short"), v0.pt());
+	  registry.fill(HIST("hEtaK0Short"), v0.eta());
+          registry.fill(HIST("hPhiK0Short"), v0.phi());
+       	}
+	if( nsigma_pos_proton < 4 && nsigma_neg_proton < 4){
+          registry.fill(HIST("hMLambda"), v0.mLambda());
+	  registry.fill(HIST("hPtLambda"), v0.pt());
+          registry.fill(HIST("hEtaLambda"), v0.eta());
+          registry.fill(HIST("hPhiLambda"), v0.phi());
+	}
+	if( nsigma_pos_pion < 4 && nsigma_neg_proton < 4){
+          registry.fill(HIST("hMAntiLambda"), v0.mAntiLambda());	
+	  registry.fill(HIST("hPtAntiLambda"), v0.pt());
+          registry.fill(HIST("hEtaAntiLambda"), v0.eta());
+          registry.fill(HIST("hPhiAntiLambda"), v0.phi());
+         }
+        // Get V0 within radius and v0cosPA
+        registry.fill(HIST("hPtTrackV0inRadius"), v0.pt());
+        registry.fill(HIST("hEtaTrackV0inRadius"), v0.eta());
+        registry.fill(HIST("hPhiTrackV0inRadius"), v0.phi());
+	}
+	registry.fill(HIST("hPtV0"), v0.pt());
+	registry.fill(HIST("hEtaV0"), v0.eta());
+        registry.fill(HIST("hPhiV0"), v0.phi());
       }	
-      for(auto& track : tracks){
+ 
+      for(auto& track : tracks){//Check kinematics of MyTracks within the sel7 process selection !
         registry.fill(HIST("hTrackPt"), track.pt());
+        registry.fill(HIST("hTrackEta"), track.eta());
+        registry.fill(HIST("hTrackPhi"), track.phi());
       }
   }
   PROCESS_SWITCH(correlationvzerojets, V0, "process v0", true);
